fa94ab5779ee1b650eca64e93c52e0b9344743f6
Diego Cuadros <cuadrosda21@gmail.com>
2024-01-29 23:35:24 -0800

    Adding finishing touches to the beginning of CCS, starting app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+//Importing CSS
+import "./App.css";
+
+//Components
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+//AboutUs
+//Book Now
+//Merch Store
+//Footer
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Fragment>
+    </Router>
   );
-}
-
-export default App;
+};
